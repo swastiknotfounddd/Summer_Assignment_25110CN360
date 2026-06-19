c036cb758ce0b3e7e02ea708ee4fd60aa8507393
swastiknotfounddd <swastikjais12@gmail.com>
2026-06-19 20:17:25 +0530

    Day 8

---
+++ Question 30.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int rows;
+
+    printf("Enter number of rows: ");
+    if (scanf("%d", &rows) != 1 || rows < 1) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    for (int i = 1; i <= rows; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Question 31.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int rows;
+
+    printf("Enter number of rows (up to 26): ");
+    if (scanf("%d", &rows) != 1 || rows < 1 || rows > 26) {
+        printf("Invalid input. Must be between 1 and 26.\n");
+        return 1;
+    }
+
+    for (int i = 1; i <= rows; ++i) {
+        for (int j = 0; j < i; ++j) {
+            printf("%c", 'A' + j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
