46c2e7d9c9366ffe4df2e843bbc879090160caf4
swastiknotfounddd <swastikjais12@gmail.com>
2026-06-19 20:40:04 +0530

    Updated Question 59

---
+++ Question 59.c
@@ -0,0 +1,50 @@
+#define __USE_MINGW_ANSI_STDIO 1
+#include <stdio.h>
+
+int main() {
+    int n, k;
+
+    printf("Enter the size of the array: ");
+    if (scanf("%d", &n) != 1 || n < 1) {
+        printf("Invalid array size.\n");
+        return 1;
+    }
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        if (scanf("%d", &arr[i]) != 1) {
+            printf("Invalid input.\n");
+            return 1;
+        }
+    }
+
+    printf("Enter number of positions to rotate right: ");
+    if (scanf("%d", &k) != 1 || k < 0) {
+        printf("Invalid rotation count.\n");
+        return 1;
+    }
+
+    k = k % n;
+
+    int temp[k];
+    for (int i = 0; i < k; i++) {
+        temp[i] = arr[n - k + i];
+    }
+
+    for (int i = n - 1; i >= k; i--) {
+        arr[i] = arr[i - k];
+    }
+
+    for (int i = 0; i < k; i++) {
+        arr[i] = temp[i];
+    }
+
+    printf("Right rotated array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
