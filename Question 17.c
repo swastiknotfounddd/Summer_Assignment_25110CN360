775c784006fead0fbaccd373915c8bf5c4363047
swastiknotfounddd <swastikjais12@gmail.com>
2026-06-19 20:39:52 +0530

    Updated Question 17

--- Question 17.c
+++ Question 17.c
@@ -1,25 +1,27 @@
+#define __USE_MINGW_ANSI_STDIO 1
+
 #include <stdio.h>
 
 int main() {
-    int num, sum = 0;
+  int num, sum = 0;
 
-    printf("Enter a positive integer: ");
-    if (scanf("%d", &num) != 1 || num <= 0) {
-        printf("Invalid input. Please enter a positive integer.\n");
-        return 1;
+  printf("Enter a positive integer: ");
+  if (scanf("%d", &num) != 1 || num <= 0) {
+    printf("Invalid input. Please enter a positive integer.\n");
+    return 1;
+  }
+
+  for (int i = 1; i <= num / 2; i++) {
+    if (num % i == 0) {
+      sum += i;
     }
+  }
 
-    for (int i = 1; i <= num / 2; i++) {
-        if (num % i == 0) {
-            sum += i;
-        }
-    }
+  if (sum == num) {
+    printf("%d is a perfect number.\n", num);
+  } else {
+    printf("%d is not a perfect number.\n", num);
+  }
 
-    if (sum == num) {
-        printf("%d is a perfect number.\n", num);
-    } else {
-        printf("%d is not a perfect number.\n", num);
-    }
-
-    return 0;
+  return 0;
 }
