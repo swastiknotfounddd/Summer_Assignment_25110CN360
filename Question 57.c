23196081319dd0b19b7b4bb96a79aa86194d8fc2
swastiknotfounddd <swastikjais12@gmail.com>
2026-06-19 20:40:04 +0530

    Updated Question 57

---
+++ Question 57.c
@@ -0,0 +1,36 @@
+#define __USE_MINGW_ANSI_STDIO 1
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter the size of the array: ");
+    if (scanf("%d", &n) != 1 || n < 1) {
+        printf("Invalid array size.\n");
+        return 1;
+    }
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        if (scanf("%d", &arr[i]) != 1) {
+            printf("Invalid input.\n");
+            return 1;
+        }
+    }
+
+    // Reverse in place
+    for (int i = 0; i < n / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+
+    printf("Reversed array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
